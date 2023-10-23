41418da60736dc920067b36b2741782f5d007674
Gagandeep Singh <deepgagan9956@gmail.com>
2023-10-23 21:54:04 +0530

    localStorage error resolved

--- src/store/todos.tsx
+++ src/store/todos.tsx
@@ -1,74 +1,96 @@
-"use client"
-
+'use client'
 import { ReactNode, createContext, useContext, useState } from "react";
 
 export type Todo = {
-    id: string;
-    task: string;
-    completed: boolean;
-    createdAt: Date;
-}
+  id: string;
+  task: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
 export type TodosContext = {
-    todos: Todo[];
-    handleAddTodo: (task: string) => void;
-    toggleTodoAsCompleted: (id: string) => void;
-    handleTodoDelete: (task: string) => void;
-    handleDeleteAll: () => void;
-}
+  todos: Todo[];
+  handleAddTodo: (task: string) => void;
+  toggleTodoAsCompleted: (id: string) => void;
+  handleTodoDelete: (id: string) => void;
+  handleDeleteAll: () => void;
+};
+
 export const todosContext = createContext<TodosContext | null>(null);
-export const TodosProvider = ({children}: {children: ReactNode}) => {
-    const [todos, setTodos] = useState<Todo[]>(()=>{
-        const newTodos = localStorage.getItem("todos") || "[]";
-        return JSON.parse(newTodos) as Todo[]; 
+
+export const TodosProvider = ({ children }: { children: ReactNode }) => {
+    const [todos, setTodos] = useState<Todo[]>(() => {
+        try{
+        const newTodos = localStorage.getItem('todos') || "[]";
+        return JSON.parse(newTodos) as Todo[]
+        }catch (e) {
+            return []
+        }
+
+    })
+  const handleAddTodo = (task: string) => {
+    setTodos((prev) => {
+      const newTodos: Todo[] = [
+        {
+          id: Math.random().toString(),
+          task,
+          completed: false,
+          createdAt: new Date(),
+        },
+        ...prev,
+      ];
+        localStorage.setItem("todos", JSON.stringify(newTodos));
+
+      return newTodos;
     });
-    const handleAddTodo = (task: string) => {
-        setTodos((prev)=>{
-            const newTodos: Todo[] = [
-                {
-                    id: Math.random().toString(),
-                    task,
-                    completed: false,
-                    createdAt: new Date()
-                },
-                    ...prev
-            ] 
-                localStorage.setItem("todos", JSON.stringify(newTodos));
-                return newTodos;
-        })
-    };
-    const toggleTodoAsCompleted = (id: string) => {
-        setTodos((prev)=>{
-            const newTodos = prev.map((task)=>{
-                if(task.id === id) {
-                    return {...task, completed: !task.completed} 
-                }
-                return task;
-            })
-            localStorage.setItem("todos", JSON.stringify(newTodos));
-            return newTodos;
-        })
-    }
-    const handleTodoDelete = (id: string) => {
-        setTodos((prev) => {
-            const newTodos = prev.filter((task)=>task.id !== id);
-            localStorage.setItem("todos", JSON.stringify(newTodos));
-            return newTodos;
-        })
-    }
-    const handleDeleteAll = () => {
-        localStorage.removeItem("todos"); 
-        setTodos([]); 
-    }
-    return(
-        <todosContext.Provider value={{todos, handleAddTodo, toggleTodoAsCompleted, handleTodoDelete, handleDeleteAll}}>
-            {children}
-        </todosContext.Provider>
-    )
-}
+  };
+
+  const toggleTodoAsCompleted = (id: string) => {
+    setTodos((prev) => {
+      const newTodos = prev.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: !task.completed };
+        }
+        return task;
+      });
+        localStorage.setItem("todos", JSON.stringify(newTodos));
+
+      return newTodos;
+    });
+  };
+
+  const handleTodoDelete = (id: string) => {
+    setTodos((prev) => {
+      const newTodos = prev.filter((task) => task.id !== id);
+        localStorage.setItem("todos", JSON.stringify(newTodos));
+      return newTodos;
+    });
+  };
+
+  const handleDeleteAll = () => {
+    localStorage.removeItem("todos");
+    setTodos([]);
+  };
+
+  return (
+    <todosContext.Provider
+      value={{
+        todos,
+        handleAddTodo,
+        toggleTodoAsCompleted,
+        handleTodoDelete,
+        handleDeleteAll,
+      }}
+    >
+      {children}
+    </todosContext.Provider>
+  );
+};
+
 export function useTodos() {
-    const todosContextValue = useContext(todosContext)
-    if(!todosContextValue){
-        throw new Error('UseTodos used outside of Provider');
-    }
-    return todosContextValue;
+  const todosContextValue = useContext(todosContext);
+  if (!todosContextValue) {
+    throw new Error('useTodos used outside of Provider');
+  }
+  return todosContextValue;
 }
